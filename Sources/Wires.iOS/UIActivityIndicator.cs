76ba08ce610b69354d1cdf51d31af8f399e9b1f3
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-05-04 15:49:47 +0200

    Add extension method for UIActivityIndicator Color
    Add extension method for UIActivityIndicator Color
    Add sample
    Prevent linking crash

--- Sources/Wires.iOS/UIActivityIndicator.cs
+++ Sources/Wires.iOS/UIActivityIndicator.cs
@@ -6,12 +6,22 @@
 
 	public static partial class UIExtensions
 	{
-		#region Image property
+		#region IsAnimating property
 
 		public static Binder<TSource, UIActivityIndicatorView> IsAnimating<TSource, TPropertyType>(this Binder<TSource, UIActivityIndicatorView> binder, Expression<Func<TSource, TPropertyType>> property, IConverter<TPropertyType, bool> converter = null)
 			where TSource : class
 		{
 			return binder.Property(property, b => b.IsAnimating, (b, v) => { if (v) b.StartAnimating(); else b.StopAnimating(); }, converter);
+		}
+
+		#endregion
+
+		#region Color property
+
+		public static Binder<TSource, UIActivityIndicatorView> Color<TSource, TPropertyType>(this Binder<TSource, UIActivityIndicatorView> binder, Expression<Func<TSource, TPropertyType>> property, IConverter<TPropertyType, UIColor> converter = null)
+			where TSource : class
+		{
+			return binder.Property(property, b => b.Color, converter);
 		}
 
 		#endregion
