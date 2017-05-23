1de794b74a3c9ab2c428dd31b90e12d20658320e
Richard Lagrange <richard.lagrange@orange.com>
2017-05-23 18:00:19 +0200

    add UIBarButton bindings

---
+++ Sources/Wires.iOS/UIBarButtonItem.cs
@@ -0,0 +1,32 @@
+﻿namespace Wires
+{
+	using System;
+	using System.Linq.Expressions;
+	using System.Windows.Input;
+	using UIKit;
+
+	public static partial class UIExtensions
+	{
+		#region TouchUpInside command
+
+		public static Binder<TSource, UIBarButtonItem> Clicked<TSource>(this Binder<TSource, UIBarButtonItem> binder, Expression<Func<TSource, ICommand>> property)
+			where TSource : class
+		{
+			return binder.Command<EventArgs>(property, nameof(UIBarButtonItem.Clicked), (bb, v) => bb.Enabled = v);
+		}
+
+		#endregion
+
+		#region Title property
+
+		public static Binder<TSource, UIBarButtonItem> Title<TSource, TPropertyType>(this Binder<TSource, UIBarButtonItem> binder, Expression<Func<TSource, TPropertyType>> property, IConverter<TPropertyType, string> converter = null)
+			where TSource : class
+		{
+			Action<UIBarButtonItem, string> setter = (b, v) => b.Title = v;
+			Func<UIBarButtonItem, string> getter = (b) => b.Title;
+			return binder.Property(property, getter, setter, converter);
+		}
+
+		#endregion
+	}
+}
